af8aa398ebcf85482e702ef2dcc38c355edb05aa
MakerSquare Student <student@lab-5.att.net>
2013-06-27 16:48:35 -0500

    Moved SQL logic to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,60 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.includes(:user).all
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @products}
+    end
+  end
+
+  def create
+    @product = Product.new(params[:product])
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created, YAYYYY!' }
+        format.json { render json: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+    @product = Product.where(id: params[:id]).first
+  end
+
+  def edit
+    @product = Product.where(id: params[:id]).first
+  end
+
+  def update
+    @product = Product.where(:id => params[:id]).first
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product = Product.where(id:params[:id]).first
+    @product.destroy
+
+    respond_to do |format|
+      format.html {redirect_to products_url, notice: "Product was successfully deleted."}
+      format.json {head :no-content}
+    end
+  end
 end
